3bc464691390de30836b1351262c5462e134299c
Alex Sitnikov <seltikc@yandex.ru>
2021-12-14 15:59:42 +0500

    Сделал модальное окно

--- js/modal.js
+++ js/modal.js
@@ -1,52 +1,8 @@
+const hamburgerButton = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
 
-// const openButton = document.querySelector("#open-modal");
-// const body = document.body;
-
-// openButton.addEventListener("click", function(event) {
-//   event.preventDefault();
-//   const overlayElement = document.createElement("div");
-//   overlayElement.classList.add("overlay__open");
-
-//   const containerElement = document.createElement("div");
-//   containerElement.classList.add("modal")
-
-//   const contentElement = document.createElement("ul");
-//   contentElement.classList.add("modal__menu")
-
-
-//   const textElement = document.createElement("li");
-//   textElement.classList.add("modal__menu-item")
-
-//   const pereBor = document.querySelectorAll("modal__menu-item");
-  
-//   for (let index = 0; index < pereBor.length; index++) {
-//     const element = pereBor[index];
-//   }
-
-//   const linkElement = document.createElement("a");
-//   linkElement.classList.add("modal__menu-link")
-//   linkElement.textContent = ("О нас");
-//   linkElement.textContent = ("Наушники");
-//   linkElement.textContent = ("Команда");
-//   linkElement.textContent = ("Цвета");
-//   linkElement.textContent = ("Отзывы");
-//   linkElement.textContent = ("Как мы работаем");
-//   linkElement.textContent = ("Контакты");
-  
-//   const closeElement = document.createElement("a")
-//   closeElement.classList.add("modal__close")
-
-//   closeElement.addEventListener("click", function(event){
-//     event.preventDefault();
-//     body.removeChild(overlayElement);
-//   })
-
-
-//   overlayElement.appendChild(containerElement);
-//   containerElement.appendChild(closeElement);
-//   containerElement.appendChild(contentElement);
-//   contentElement.appendChild(textElement);
-//   textElement.appendChild(linkElement);
-
-//   body.appendChild(overlayElement);
-// })
+hamburgerButton.addEventListener("click", function(){
+  menu.classList.toggle("overlay--active");
+  hamburgerButton.classList.toggle("hamburger--active");
+  hamburgerButton.classList.toggle("hamburger");
+})
