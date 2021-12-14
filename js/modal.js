4179ce8d399e56e65d595cd657055bc65d68ec70
Alex Sitnikov <seltikc@yandex.ru>
2021-12-14 14:35:35 +0500

    Модалка

--- js/modal.js
+++ js/modal.js
@@ -1,37 +1,52 @@
-const openButton = document.querySelector("#open-modal");
-const body = document.body;
 
-openButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  const overlayElement = document.createElement("div");
-  overlayElement.classList.add("overlay__open");
+// const openButton = document.querySelector("#open-modal");
+// const body = document.body;
 
-  const containerElement = document.createElement("div");
-  containerElement.classList.add("modal")
+// openButton.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   const overlayElement = document.createElement("div");
+//   overlayElement.classList.add("overlay__open");
 
-  const contentElement = document.createElement("ul");
-  contentElement.classList.add("modal__menu")
+//   const containerElement = document.createElement("div");
+//   containerElement.classList.add("modal")
 
-  const textElement = document.createElement("li");
-  textElement.classList.add("modal__menu-item")
-
-  const linkElement = document.createElement("a");
-  linkElement.classList.add("modal__menu-link")
-
-  const closeElement = document.createElement("a")
-  closeElement.classList.add("modal__close")
-
-  closeElement.addEventListener("click", function(event){
-    event.preventDefault();
-    body.removeChild(overlayElement);
-  })
+//   const contentElement = document.createElement("ul");
+//   contentElement.classList.add("modal__menu")
 
 
-  overlayElement.appendChild(containerElement);
-  containerElement.appendChild(closeElement);
-  containerElement.appendChild(contentElement);
-  contentElement.appendChild(textElement);
-  textElement.appendChild(linkElement);
+//   const textElement = document.createElement("li");
+//   textElement.classList.add("modal__menu-item")
 
-  body.appendChild(overlayElement);
-})
+//   const pereBor = document.querySelectorAll("modal__menu-item");
+  
+//   for (let index = 0; index < pereBor.length; index++) {
+//     const element = pereBor[index];
+//   }
+
+//   const linkElement = document.createElement("a");
+//   linkElement.classList.add("modal__menu-link")
+//   linkElement.textContent = ("О нас");
+//   linkElement.textContent = ("Наушники");
+//   linkElement.textContent = ("Команда");
+//   linkElement.textContent = ("Цвета");
+//   linkElement.textContent = ("Отзывы");
+//   linkElement.textContent = ("Как мы работаем");
+//   linkElement.textContent = ("Контакты");
+  
+//   const closeElement = document.createElement("a")
+//   closeElement.classList.add("modal__close")
+
+//   closeElement.addEventListener("click", function(event){
+//     event.preventDefault();
+//     body.removeChild(overlayElement);
+//   })
+
+
+//   overlayElement.appendChild(containerElement);
+//   containerElement.appendChild(closeElement);
+//   containerElement.appendChild(contentElement);
+//   contentElement.appendChild(textElement);
+//   textElement.appendChild(linkElement);
+
+//   body.appendChild(overlayElement);
+// })
