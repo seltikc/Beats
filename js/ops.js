5715c49ea471030d444654f0483f90808993c27c
Alex Sitnikov <seltikc@yandex.ru>
2021-12-26 16:12:09 +0500

    Работа над ошибками

--- js/ops.js
+++ js/ops.js
@@ -1,5 +1,7 @@
 const section = $("section");
 const display = $(".maincontent");
+const mobileDetect = new MobileDetect(window.navigator.userAgent);
+const isMobile = mobileDetect.mobile();
 
 let inScroll = false;
 
@@ -10,6 +12,15 @@ const performTransition = sectionEq => {
   if(inScroll === false) {
     inScroll = true;
     const position = sectionEq * -100;
+    const currentSection = section.eq(sectionEq);
+    const menuTheme = currentSection.attr("data-sidemenu-theme");
+    const sideMenu = $(".fixed-menu");
+
+    if(menuTheme !== "black") {
+      sideMenu.addClass("fixed-menu--white");
+    }else {
+      sideMenu.removeClass("fixed-menu--white");
+    }
 
   display.css({
     transform: `translateY(${position}%)`,
@@ -19,6 +30,8 @@ const performTransition = sectionEq => {
 
   setTimeout(() => {
     inScroll = false;
+    sideMenu.find(".fixed-menu__item").eq(sectionEq).addClass("fixed-menu__item--active").siblings().removeClass("fixed-menu__item--active");
+
   }, 1300);
   } 
 };
@@ -65,5 +78,34 @@ $(window).on("keydown", e => {
     }
   }
 
+});
 
-})
+$(".wrapper").on("touchmove", e => e.preventDefault());
+
+$("[data-scroll-to]").click(e => {
+  e.preventDefault();
+
+  const $this = $(e.currentTarget);
+  const target = $this.attr("data-scroll-to");
+  const reqSection = $(`[data-section-id=${target}]`);
+
+  performTransition(reqSection.index());
+});
+
+// https://github.com/mattbryson/TouchSwipe-Jquery-Plugin
+
+if (isMobile) {
+  $("body").swipe( {
+    swipe: function(event, direction) {
+      const scroller = viewportScroller()
+      let scrollDirection = "";
+
+      if (direction === "up") scrollDirection = "next";
+      if (direction === "down") scrollDirection = "prev";
+
+      scroller[scrollDirection]();
+    }
+  });
+}
+
+
